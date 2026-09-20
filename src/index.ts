e63af3be47ab983b4947b11c71caf033e64b43de
Mariusz Złotucha <mariusz.zlotucha@gmail.com>
2026-09-20 15:51:17 +0200

    import langchain

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('test') 
+import { ChatOpenAI } from "@langchain/openai";
+import { createAgent } from "langchain";
+
+const model = new ChatOpenAI({
+    apiKey: process.env.OPENROUTER_API_KEY,
+    model: 'openrouter/free',
+    temperature: 0.7,
+    configuration: {
+        baseURL: 'https://openrouter.ai/api/v1'
+    }
+
+})
+
+const landingPageAgent = createAgent({
+    model,
+    tools: [],
+    systemPrompt: 'You are web designer and developer who creates clear, conversion-focued landing pages'
+})
+
