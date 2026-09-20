4c729edef0d81448eb27ea8154ac4e8385900abf
Mariusz Złotucha <mariusz.zlotucha@gmail.com>
2026-09-20 20:41:57 +0200

    add save index html tool

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { input } from "@inquirer/prompts";
 import { ChatOpenAI } from "@langchain/openai";
 import { createAgent } from "langchain";
+import {tool} from '@langchain/core/tools'
+import {writeFile} from 'node:fs/promises'
+import {resolve} from 'node:path';
+import {z } from 'zod';
 
 const model = new ChatOpenAI({
     apiKey: process.env.OPENROUTER_API_KEY,
@@ -12,10 +16,24 @@ const model = new ChatOpenAI({
 
 })
 
+const saveIndexHtml = tool(
+    async ({html}) => {
+        const outputPath = resolve(process.cwd(), 'index.html')
+        await writeFile(outputPath, html, 'utf-8')
+        return `saved to ${outputPath}`
+    }, {
+        name: 'save_index_html',
+        description: 'Save a complete landing page as index.html in the current project',
+        schema: z.object({
+            html: z.string().describe('The complete raw HTML document, including CSS and scripts')
+        })
+    }
+)
+
 const landingPageAgent = createAgent({
     model,
-    tools: [],
-    systemPrompt: 'You are web designer and developer who creates clear, conversion-focued landing pages'
+    tools: [saveIndexHtml],
+    systemPrompt: 'You are web designer and developer who creates clear, conversion-focued landing pages. Generate a complete, self-contained HTML docuemnt based ont the users request. You must call save_index_html with finished HTML so it is written to disc as index.html. Do not wrap the HTML in Markdown fences'
 })
 
 const prompt = await input({
