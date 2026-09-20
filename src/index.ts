1a84d57aff2909f8870d1da00d3c4a8ccd1c6875
Mariusz Złotucha <mariusz.zlotucha@gmail.com>
2026-09-20 16:06:48 +0200

    import langchain

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+import { input } from "@inquirer/prompts";
 import { ChatOpenAI } from "@langchain/openai";
 import { createAgent } from "langchain";
 
 const model = new ChatOpenAI({
     apiKey: process.env.OPENROUTER_API_KEY,
-    model: 'openrouter/free',
+    model: 'openai/gpt-5.6-luna',
     temperature: 0.7,
     configuration: {
         baseURL: 'https://openrouter.ai/api/v1'
@@ -17,3 +18,14 @@ const landingPageAgent = createAgent({
     systemPrompt: 'You are web designer and developer who creates clear, conversion-focued landing pages'
 })
 
+const prompt = await input({
+    message: 'Your prompt: '
+})
+
+const result = await landingPageAgent.invoke({
+    messages: [{
+        role: 'user', content: prompt
+    }]
+})
+
+console.log(result.messages.at(-1)?.content);
